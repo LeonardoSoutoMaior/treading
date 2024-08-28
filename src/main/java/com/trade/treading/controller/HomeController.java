bbcd9c3feb7960b4e52dd3c72fc7bc1142e3584a
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-08-28 10:47:05 -0300

    Configurações iniciais

---
+++ src/main/java/com/trade/treading/controller/HomeController.java
@@ -0,0 +1,4 @@
+package com.trade.treading.controller;
+
+public class HomeController {
+}
---
+++ src/test/java/com/trade/treading/TreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.treading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
